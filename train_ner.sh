0b15f146bb46dc38c1cc90fcc3cad91e3367e2b6
Raj Nath Patel <patelrajnath@gmail.com>
2021-04-04 19:17:42 +0000

    Add shell scripts.

--- train_ner.sh
+++ train_ner.sh
@@ -1,4 +1,4 @@
-python -m examples.cli --model-file=/RAJ/luke_base_500k.tar.gz --output-dir=model ner run --train-batch-size 8
+CUDA_VISIBLE_DEVICES=1 python -m examples.cli --model-file=/RAJ/luke_base_500k.tar.gz --output-dir=model_original_run3 ner run --train-batch-size 2
 #python -m examples.cli --model-file=/RAJ/luke_base_500k_kg_pretrain.tar.gz --output-dir=model ner run --train-batch-size 8
 
 #python -m examples.cli --model-file=/RAJ/luke_base_500k.tar.gz --output-dir=model ner run --train-batch-size 8 --data-dir data/nlu
